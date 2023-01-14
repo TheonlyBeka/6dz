05dca6c5e251bf24fbf99cd168e7bea3c05d2530
TheonlyBeka <theonly.bekzat@gmail.com>
2023-01-14 16:02:55 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import CalcPage from "./calcPage/CalcPage";
+
+function App() {
+  return (
+    <div className="App">
+      <CalcPage/>
+    </div>
+  );
+}
+
+export default App;
